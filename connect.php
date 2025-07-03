59db55863297168cf8c878ce5c5dd7b50d16a5f9
Ziyad Alwafie <zyalwfie@gmail.com>
2025-07-03 09:36:26 +0800

    chore: empty the password for localhost

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('localhost', 'root', 'root', 'apotek_alifa');
+$conn = new mysqli('localhost', 'root', '', 'apotek_alifa');
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
